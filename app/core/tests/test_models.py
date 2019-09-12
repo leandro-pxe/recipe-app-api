6328e9aca1099f8e15172eab6e05aaa16026fa0c
Leandro <leandro_pxe@yahoo.com.br>
2019-09-12 18:20:17 -0300

    Added recipe app with list tag endpont

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
-
 from django.contrib.auth import get_user_model
+
+from .. import models
+
+
+def sample_user(email='teste@ciaoitalia.it', password='IlToro',):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -38,3 +44,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegana'
+        )
+
+        self.assertEqual(str(tag), tag.name)
