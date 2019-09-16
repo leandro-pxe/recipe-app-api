39e25fb927117bdf8d69e34eb6dec6345990aa16
Leandro <leandro_pxe@yahoo.com.br>
2019-09-16 19:31:22 -0300

    added create ingredient feature

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -65,21 +65,21 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ing_mine.name)
 
-    # def test_create_ingredients_successful(self):
-    #     payload = {'name': 'Rice'}
-    #     res = self.client.post(INGREDIENTS_URL, payload)
-    #
-    #     exists = Ingredient.objects.filter(
-    #         user=self.user,
-    #         name=payload['name']
-    #     ).exists()
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-    #     self.assertTrue(exists)
-    #
-    # def test_create_ingredients_invalid(self):
-    #     payload = {'name': ''}
-    #
-    #     res = self.client.post(INGREDIENTS_URL, payload)
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_create_ingredients_successful(self):
+        payload = {'name': 'Rice'}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(exists)
+
+    def test_create_ingredients_invalid(self):
+        payload = {'name': ''}
+
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
