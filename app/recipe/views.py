4e9a4d6366dc92ef178647c23235cf90e352252e
Leandro <leandro_pxe@yahoo.com.br>
2019-09-18 16:33:46 -0300

    pep8

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,8 +8,8 @@ from . import serializers
 
 # TODO learn more about viewset and mixins
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
     """Base viewset for user owned recipe attributes"""
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
